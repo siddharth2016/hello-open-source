65358302e1f1cc76d41da6859990b68bc4a23039
Luvdipsingh Dadiyala <ldadiyala@gmail.com>
2021-10-02 11:34:04 +0530

    create hello_luv2001.cpp

---
+++ hello/hello_luv2001.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define db1(x) cout<<#x<<"="<<x<<'\n'
+#define db2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<'\n'
+#define db3(x,y,z) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<","<<#z<<"="<<z<<'\n'
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define repA(i,a,n) for(int i=a;i<=(n);++i)
+#define repD(i,a,n) for(int i=a;i>=(n);--i)
+#define push_back pb
+using ll = long long;
+
+
+using namespace std;
+using ll = long long;
+
+/*
+	Luvdipsingh Dadiyala
+*/
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cout << "Hello Open Source" << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
